7a68cffded9cfee9c03d27a4a6a0569768aa9428
unknown <volkamrpadierna@gmail.com>
2021-09-30 23:23:36 -0500

    version 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let pokemon1, pokemon2;
 let name1, name2;
+let altura1, altura2;
+let peso1, peso2;
 
 function duelo(){
     let aleatorio = Math.round(Math.random()*(100-1)+parseInt(1));;
@@ -8,16 +10,22 @@ function duelo(){
     name1 = document.getElementById("duelo1p");
     pokemon2 = document.getElementById("duelo2");
     name2 = document.getElementById("duelo2p");
-    traerDatos(pokemon1,name1,aleatorio);
-    traerDatos(pokemon2,name2,aleatorio2);
+    altura1 = document.getElementById("dueloal");
+    altura2 = document.getElementById("dueloal2");
+    peso1 = document.getElementById("duelope");
+    peso2 = document.getElementById("duelope2");
+    traerDatos(pokemon1,name1,altura1,peso1,aleatorio);
+    traerDatos(pokemon2,name2,altura2,peso2,aleatorio2);
 }
 
-function traerDatos(contenedor,nombre,id){
+function traerDatos(contenedor,nombre,altura,peso,id){
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then(response => response.json())
     .then(data => {
       contenedor.src = data.sprites.front_default;
       nombre.innerHTML = data.name;
+      altura.innerHTML = "altura: " + data.height;
+      peso.innerHTML = "peso: " + data.weight;
      })
 }
 
@@ -37,9 +45,19 @@ function equipo(){
     name3 = document.getElementById("team3p");
     name4 = document.getElementById("team4p");
 
-    traerDatos(pokemon1,name1,aleatoriot);
-    traerDatos(pokemon2,name2,aleatoriot2);
-    traerDatos(pokemon3,name3,aleatoriot3);
-    traerDatos(pokemon4,name4,aleatoriot4);
+    peso1 = document.getElementById("teampe1");
+    peso2 = document.getElementById("teampe2");
+    peso3 = document.getElementById("teampe3");
+    peso4 = document.getElementById("teampe4");
+
+    altura1 = document.getElementById("teamal1");
+    altura2 = document.getElementById("teamal2");
+    altura3 = document.getElementById("teamal3");
+    altura4 = document.getElementById("teamal4");
+
+    traerDatos(pokemon1,name1,altura1,peso1,aleatoriot);
+    traerDatos(pokemon2,name2,altura2,peso2,aleatoriot2);
+    traerDatos(pokemon3,name3,altura3,peso3,aleatoriot3);
+    traerDatos(pokemon4,name4,altura4,peso4,aleatoriot4);
 
 }
