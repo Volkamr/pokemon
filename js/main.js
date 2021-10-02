e852a67c48ea42fbb11cd76e8dadc216fe2c45d3
unknown <volkamrpadierna@gmail.com>
2021-10-02 00:26:37 -0500

    version 4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let pokemon1, pokemon2;
 let name1, name2;
-let altura1, altura2;
-let peso1, peso2;
+let ataque1, ataque2;
+let defensa1, defesensa2;
 
 function duelo(){
     let aleatorio = Math.round(Math.random()*(100-1)+parseInt(1));;
@@ -10,22 +10,22 @@ function duelo(){
     name1 = document.getElementById("duelo1p");
     pokemon2 = document.getElementById("duelo2");
     name2 = document.getElementById("duelo2p");
-    altura1 = document.getElementById("dueloal");
-    altura2 = document.getElementById("dueloal2");
-    peso1 = document.getElementById("duelope");
-    peso2 = document.getElementById("duelope2");
-    traerDatos(pokemon1,name1,altura1,peso1,aleatorio);
-    traerDatos(pokemon2,name2,altura2,peso2,aleatorio2);
+    ataque1 = document.getElementById("dueloa");
+    ataque2 = document.getElementById("dueloa2");
+    defensa1 = document.getElementById("duelod");
+    defensa2 = document.getElementById("duelod2");
+    traerDatos(pokemon1,name1,ataque1,defensa1,aleatorio);
+    traerDatos(pokemon2,name2,ataque2,defensa2,aleatorio2);
 }
 
-function traerDatos(contenedor,nombre,altura,peso,id){
+function traerDatos(contenedor,nombre,ataque,defensa,id){
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then(response => response.json())
     .then(data => {
       contenedor.src = data.sprites.front_default;
       nombre.innerHTML = data.name;
-      altura.innerHTML = "altura: " + data.height;
-      peso.innerHTML = "peso: " + data.weight;
+      ataque.innerHTML = "Attack: " + data.stats[1].base_stat;
+      defensa.innerHTML = "Defense: " + data.stats[2].base_stat;
      })
 }
 
@@ -45,19 +45,52 @@ function equipo(){
     name3 = document.getElementById("team3p");
     name4 = document.getElementById("team4p");
 
-    peso1 = document.getElementById("teampe1");
-    peso2 = document.getElementById("teampe2");
-    peso3 = document.getElementById("teampe3");
-    peso4 = document.getElementById("teampe4");
+    ataque1 = document.getElementById("teama1");
+    ataque2 = document.getElementById("teama2");
+    ataque3 = document.getElementById("teama3");
+    ataque4 = document.getElementById("teama4");
 
-    altura1 = document.getElementById("teamal1");
-    altura2 = document.getElementById("teamal2");
-    altura3 = document.getElementById("teamal3");
-    altura4 = document.getElementById("teamal4");
+    defensa1 = document.getElementById("teamd1");
+    defensa2 = document.getElementById("teamd2");
+    defensa3 = document.getElementById("teamd3");
+    defensa4 = document.getElementById("teamd4");
 
-    traerDatos(pokemon1,name1,altura1,peso1,aleatoriot);
-    traerDatos(pokemon2,name2,altura2,peso2,aleatoriot2);
-    traerDatos(pokemon3,name3,altura3,peso3,aleatoriot3);
-    traerDatos(pokemon4,name4,altura4,peso4,aleatoriot4);
+    traerDatos(pokemon1,name1,ataque1,defensa1,aleatoriot);
+    traerDatos(pokemon2,name2,ataque2,defensa2,aleatoriot2);
+    traerDatos(pokemon3,name3,ataque3,defensa3,aleatoriot3);
+    traerDatos(pokemon4,name4,ataque4,defensa4,aleatoriot4);
 
+}
+
+let imagen, nombre, vida, ataque, defensa, ataquee, defensae, velocidad;
+
+function poketotal(){
+
+    imagen = document.getElementById("pokecomp");
+    nombre = document.getElementById("nombre");
+    vida = document.getElementById("vida");
+    ataque = document.getElementById("ataque");
+    defensa = document.getElementById("defensa");
+    ataquee = document.getElementById("ataque especial");
+    defensae = document.getElementById("defensa especial");
+    velocidad = document.getElementById("velocidad");
+    id = Math.round(Math.random()*(100-5)+parseInt(1));
+
+    estado(imagen,vida,ataque,defensa,ataquee,defensae,velocidad,id);
+
+}
+
+function estado(imagen,vida,ataque,defensa,ataquee,defensae,velocidad,id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      nombre.innerHTML = data.name;
+      imagen.src = data.sprites.front_default;
+      vida.innerHTML = "Hp: " + data.stats[0].base_stat;
+      ataque.innerHTML = "Attack: " + data.stats[1].base_stat;
+      defensa.innerHTML = "Defense: " + data.stats[2].base_stat;
+      ataquee.innerHTML = "Special Attack: " + data.stats[3].base_stat;
+      defensae.innerHTML = "Special Defense" + data.stats[4].base_stat;
+      velocidad.innerHTML = "Speed: " + data.stats[5].base_stat;
+     })
 }
