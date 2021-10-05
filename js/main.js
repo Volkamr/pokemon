2a7b2ebf7bd31474fa6d70043c104ea0f2eacd9f
unknown <volkamrpadierna@gmail.com>
2021-10-05 16:00:38 -0500

    version final

--- js/main.js
+++ js/main.js
@@ -2,6 +2,26 @@ let pokemon1, pokemon2;
 let name1, name2;
 let ataque1, ataque2;
 let defensa1, defesensa2;
+let fondofondo, fondofondo2;
+
+
+const colors = {
+	fire: '#fca311',
+	grass: '#606c38',
+	electric: '#deff0a',
+	water: '#219ebc',
+	ground: '#283618',
+	rock: '#dda15e', 
+	fairy: '#ccd5ae',
+	poison: '#023047',
+	bug: '#f4a261',
+	dragon: '#ffb703',
+	psychic: '#ccc5b9',
+	flying: '#8ecae6',
+	fighting: '#ee6c4d',
+	normal: '#e09f3e',
+    ghost: '#e8dab2',
+};
 
 function duelo(){
     let aleatorio = Math.round(Math.random()*(100-1)+parseInt(1));;
@@ -14,8 +34,12 @@ function duelo(){
     ataque2 = document.getElementById("dueloa2");
     defensa1 = document.getElementById("duelod");
     defensa2 = document.getElementById("duelod2");
+    fondofondo = document.getElementById("f");
+    fondofondo2 = document.getElementById("f2");
     traerDatos(pokemon1,name1,ataque1,defensa1,aleatorio);
     traerDatos(pokemon2,name2,ataque2,defensa2,aleatorio2);
+    fondo(fondofondo, aleatorio);
+    fondo(fondofondo2, aleatorio2);
 }
 
 function traerDatos(contenedor,nombre,ataque,defensa,id){
@@ -55,10 +79,20 @@ function equipo(){
     defensa3 = document.getElementById("teamd3");
     defensa4 = document.getElementById("teamd4");
 
+    backg = document.getElementById("t1");
+    backg2 = document.getElementById("t2");
+    backg3 = document.getElementById("t3");
+    backg4 = document.getElementById("t4");
+
     traerDatos(pokemon1,name1,ataque1,defensa1,aleatoriot);
     traerDatos(pokemon2,name2,ataque2,defensa2,aleatoriot2);
     traerDatos(pokemon3,name3,ataque3,defensa3,aleatoriot3);
     traerDatos(pokemon4,name4,ataque4,defensa4,aleatoriot4);
+
+    fondo(backg,aleatoriot);
+    fondo(backg2,aleatoriot2);
+    fondo(backg3,aleatoriot3);
+    fondo(backg4,aleatoriot4);
 
 }
 
@@ -77,7 +111,7 @@ function poketotal(){
     id = Math.round(Math.random()*(100-5)+parseInt(1));
 
     estado(imagen,vida,ataque,defensa,ataquee,defensae,velocidad,id);
-
+    fondo(imagen,id);
 }
 
 function estado(imagen,vida,ataque,defensa,ataquee,defensae,velocidad,id){
@@ -93,4 +127,20 @@ function estado(imagen,vida,ataque,defensa,ataquee,defensae,velocidad,id){
       defensae.innerHTML = "Special Defense" + data.stats[4].base_stat;
       velocidad.innerHTML = "Speed: " + data.stats[5].base_stat;
      })
+}
+
+function fondo(contenedor,id){
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        const {types} = data;
+        console.log(data)
+        setCardColor(types);
+     })
+
+    const setCardColor = types => {
+        const colorOne = colors[types[0].type.name];
+        contenedor.style.backgroundColor =  colorOne;
+    }
 }
